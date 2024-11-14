3c283f64a5facef859b0857563307b8e2d1a32fc
drswinghead <drswinghead@163.com>
2024-11-14 11:30:33 +0800

    som

--- mpvtk/main.v
+++ mpvtk/main.v
@@ -4,6 +4,8 @@ import os
 import vcp
 import  vcp.tcltk
 import  vcp.mpv
+// 不链接libmpv 30M, 链接了之后90M,初始化后??M,播放时120M
+// 单纯的 mpv 窗口 108M
 
 struct Globvars {
 	pub mut:
@@ -53,6 +55,7 @@ fn create_btns() {
 		lb.pack(btn)
 		btn.connect(fn(cbval voidptr, args []string){
 			vcp.info("hehhe", cbval, args.str())
+			create_mpvobj(gvars.wid)
 		}, vnil)
 	}
 
@@ -68,12 +71,12 @@ fn create_btns() {
 	frm := tcltk.Frame.new()
 	lb.pack(frm)
 
-	wid := frm.id()
+	wid := frm.id().clone()
 	vcp.info("wid", wid, frm.name())
 	gv := gvars
 	gv.wid = wid 
 
-	create_mpvobj(wid)
+	// create_mpvobj(wid)
 }
 
 fn create_frms() {
@@ -91,7 +94,9 @@ fn create_mpvobj(wid string) {
 	mut rv := 0
 
 	vcp.info(gvars.wid, gvars.wid.len, gv.mpvo)
+	assert gv.wid.starts_with('0x')
 	rv = C.mpv_set_option_string(h, c'wid', gvars.wid.str)
+	// rv = C.mpv_set_option_string(h, c'wid', c'0x180001a')
 	check_mpvret(rv)
 
 	opts := ["wid", wid] // "vo", "xv"
@@ -99,7 +104,7 @@ fn create_mpvobj(wid string) {
 		// rv = C.mpv_set_option_string(h, opts[i].str, opts[i+1].str)
 		// vcp.info(i.str(), rv, opts[i], opts[i+1])
 	}
-	// rv = C.mpv_set_option_string(h, c'vo', c'xv')
+	rv = C.mpv_set_option_string(h, c'vo', c'xv')
 	// vcp.info(i.str(), rv, opts[i], opts[i+1])
 	check_mpvret(rv)
 
@@ -114,8 +119,11 @@ fn create_mpvobj(wid string) {
 	// 尝试一种可能的解决方案,用xlib创建窗口,看能不能放到tcltk的layout中
 	mut loadfile := "loadfile "
 	loadfile += if os.args.len>1{ os.args[1]} else {"hello.mp4"}
-	rv = C.mpv_command_string(h, loadfile.str)
+	// rv = C.mpv_command_string(h, loadfile.str)
 	// rv = C.mpv_command_string(h, c"loadfile hello.mp4")
+
+	cmdargs := [charptr('loadfile'.str), charptr(os.args[1].str), vnil]
+	rv = C.mpv_command_async(h, 12345, cmdargs.data)
 	vcp.info(rv)
 	check_mpvret(rv)
 }
