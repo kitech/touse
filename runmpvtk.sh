fcd48271231762018c790f1567b8c34f96f5e054
egitop <egitop@uload.xyz>
2025-10-10 13:48:42 +0800

    lit dylib

--- runmpvtk.sh
+++ runmpvtk.sh
@@ -4,4 +4,14 @@
 shdir=$(dirname $0)
 cd $shdir
 
-$shdir/mpvtk/mpvtk "$@" > $shdir/mpvtk.log 2>&1
+# 源码数模式运行
+if [ -d $shdir/mpvtk ]; then
+    export LD_LIBRARY_PATH=$shdir/mpvtk
+    $shdir/mpvtk/mpvtk "$@" > $shdir/mpvtk.log 2>&1
+elif [ -f $shdir/mpvtk ]; then
+    # 打包运行，本脚本和exe在同一个目录
+    export LD_LIBRARY_PATH=$shdir
+    $shdir/mpvtk "$@" > $shdir/mpvtk.log 2>&1
+else
+    echo "mpvtk executable not found"
+fi
