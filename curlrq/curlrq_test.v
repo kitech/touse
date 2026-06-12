1d21009d0cc6b6e7bc5ae1c67956f09ce9b6a5e2
gitsplit <gitsplit@wtmail.com>
2026-06-12 21:45:13 +0800

    impr tests

--- curlrq/curlrq_test.v
+++ curlrq/curlrq_test.v
@@ -1,42 +1,28 @@
 module curlrq
 
-struct TestResult {
-	id       int
-	http_code int
-	body_len int
-}
-
-struct TestCtx {
-	ch chan TestResult
-	id int
-}
-
-fn on_request_done(resp &C.curlrq_response_t, user_data voidptr) {
-	ctx := &TestCtx(user_data)
-	ctx.ch <- TestResult{
-		id: ctx.id
-		http_code: resp.http_code
-		body_len: int(resp.response_body_len)
-	}
-	C.curlrq_response_free(resp)
-}
-
-fn test_echo_requests() {
-	ch := chan TestResult{cap: 4}
-
+fn test_get_request() {
 	eng := new_engine(3) or { panic(err) }
 	defer {
 		eng.cleanup()
 	}
-
 	eng.add(Request{
-		url: 'https://postman-echo.com/get'
+		url: 'https://postman-echo.com/get?q=hello'
 		method: 'GET'
 		timeout_ms: 10000
 		verify_ssl: false
-		user_data: &TestCtx{ch, 1}
-	}, on_request_done) or { panic(err) }
+	}, fn (resp &C.curlrq_response_t, _ voidptr) {
+		assert resp.http_code == 200
+		assert resp.response_body_len > 0
+		C.curlrq_response_free(resp)
+	}) or { panic(err) }
+	eng.wait()
+}
 
+fn test_post_request() {
+	eng := new_engine(3) or { panic(err) }
+	defer {
+		eng.cleanup()
+	}
 	eng.add(Request{
 		url: 'https://postman-echo.com/post'
 		method: 'POST'
@@ -44,9 +30,19 @@ fn test_echo_requests() {
 		headers: ['Content-Type: application/json', 'Accept: application/json']
 		timeout_ms: 10000
 		verify_ssl: false
-		user_data: &TestCtx{ch, 2}
-	}, on_request_done) or { panic(err) }
+	}, fn (resp &C.curlrq_response_t, _ voidptr) {
+		assert resp.http_code == 200
+		assert resp.response_body_len > 0
+		C.curlrq_response_free(resp)
+	}) or { panic(err) }
+	eng.wait()
+}
 
+fn test_put_request() {
+	eng := new_engine(3) or { panic(err) }
+	defer {
+		eng.cleanup()
+	}
 	eng.add(Request{
 		url: 'https://postman-echo.com/put'
 		method: 'PUT'
@@ -54,27 +50,28 @@ fn test_echo_requests() {
 		headers: ['Content-Type: text/plain']
 		timeout_ms: 10000
 		verify_ssl: false
-		user_data: &TestCtx{ch, 3}
-	}, on_request_done) or { panic(err) }
+	}, fn (resp &C.curlrq_response_t, _ voidptr) {
+		assert resp.http_code == 200
+		assert resp.response_body_len > 0
+		C.curlrq_response_free(resp)
+	}) or { panic(err) }
+	eng.wait()
+}
 
+fn test_delete_request() {
+	eng := new_engine(3) or { panic(err) }
+	defer {
+		eng.cleanup()
+	}
 	eng.add(Request{
 		url: 'https://postman-echo.com/delete'
 		method: 'DELETE'
 		timeout_ms: 10000
 		verify_ssl: false
-		user_data: &TestCtx{ch, 4}
-	}, on_request_done) or { panic(err) }
-
+	}, fn (resp &C.curlrq_response_t, _ voidptr) {
+		assert resp.http_code == 200
+		assert resp.response_body_len > 0
+		C.curlrq_response_free(resp)
+	}) or { panic(err) }
 	eng.wait()
-
-	mut results := []TestResult{}
-	for _ in 0 .. 4 {
-		results << <-ch
-	}
-
-	assert results.len == 4
-	for r in results {
-		assert r.http_code == 200, 'req ${r.id}: expected 200, got ${r.http_code}'
-		assert r.body_len > 0, 'req ${r.id}: body should not be empty'
-	}
 }
